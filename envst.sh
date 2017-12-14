f69f3761048b9f76adbaae5193542e0f2740baf4
Cristian Stroparo <cstropz@gmail.com>
2017-12-13 22:48:46 -0200

    Added post calls to envst

--- envst.sh
+++ envst.sh
@@ -3,3 +3,8 @@ set -o vi
 if [ -e /c/opt/subl ] ; then
   pathmunge -x /c/opt/subl
 fi
+
+if [[ "$(uname -a)" = *[Ll]inux* ]] ; then
+  appendto DS_POST_CALLS '[[ $- = *i* ]] && echo 1>&2 && d "${DEV:-$HOME/workspace}"; true'
+  appendto DS_POST_CALLS '[[ $- = *i* ]] && echo 1>&2 && [ -d ds ] && gitr.sh ss; true'
+fi
