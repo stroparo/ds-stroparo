dce6560b9fd83b8922dd834fa2ee706e1a4c4e98
Cristian Stroparo <cstropz@gmail.com>
2017-12-19 14:07:05 +0000

    Call czbash in envst

--- envst.sh
+++ envst.sh
@@ -4,6 +4,10 @@ if [ -e /c/opt/subl ] ; then
   pathmunge -x /c/opt/subl
 fi
 
+if [ -n "$BASH_VERSION" ] ; then
+  czbash
+fi
+
 if [[ "$(uname -a)" = *[Ll]inux* ]] ; then
   appendto DS_POST_CALLS '[[ $- = *i* ]] && echo 1>&2 && d "${DEV:-$HOME/workspace}"; true'
   appendto DS_POST_CALLS '[[ $- = *i* ]] && echo 1>&2 && [ -d ds ] && gitr.sh ss; true'
