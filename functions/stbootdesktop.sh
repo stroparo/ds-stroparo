a5e4bfa8e870bea470d2254c62f87dd588a24fd5
Cristian Stroparo <cstropz@gmail.com>
2018-01-04 15:01:27 -0200

    Idempotent

--- functions/stbootdesktop.sh
+++ functions/stbootdesktop.sh
@@ -10,12 +10,12 @@ bootdesktop () {
   /usr/bin/setxkbmap -option "ctrl:nocaps"
 
   # Quake style terminal emulator:
-  if ! pgrep -fl guake ; then
+  if type guake >/dev/null 2>&1 && ! pgrep -fl guake ; then
     guake & disown
   fi
 
   # Display lighting following the sun at Curitiba, Brazil:
-  if type flux >/dev/null 2>&1 ; then
+  if type flux >/dev/null 2>&1 && ! pgrep -fl flux; then
     flux -l -25 -g -49 -k 4700 & disown
   fi
 
