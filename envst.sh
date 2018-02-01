a61e8548769964249dd6f4c7e4db3377ec98147f
Cristian Stroparo <cstropz@gmail.com>
2018-01-31 23:53:17 -0200

    Removed unnecessary munging (mungemagic already does these)

--- envst.sh
+++ envst.sh
@@ -36,27 +36,18 @@ mungemagic /opt
 # #############################################################################
 # Sublime
 
-if [ -e ~/opt/subl ] ; then pathmunge -x ~/opt/subl ; fi
-if [ -e /cygdrive/c/opt/subl ] ; then pathmunge -x /c/opt/subl ; fi
-
 if ! which subl >/dev/null 2>&1 && which sublime_text >/dev/null 2>&1 ; then
   sudo ln "$(which sublime_text)" /usr/local/bin/subl
 fi
 
 # #############################################################################
-# VSCode
+# Default editors
 
-if [ -e ~/opt/vscode ] ; then pathmunge -x ~/opt/vscode ; fi
-if [ -e /cygdrive/c/opt/vscode ] ; then pathmunge -x /c/opt/vscode ; fi
-
-# #############################################################################
-# Default GUI editor
+export GIT_EDITOR=vim
 
 if which subl >/dev/null 2>&1 ; then
-  export GIT_EDITOR="subl --wait --new-window"
   export VISUAL=subl
 elif which Code >/dev/null 2>&1 ; then
-  export GIT_EDITOR="Code"
   export VISUAL=Code
 fi
 
