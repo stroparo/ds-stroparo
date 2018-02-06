5ec8a425d50a78d5582d8f992aafa5d9aa7e3e93
Cristian Stroparo <cstropz@gmail.com>
2018-02-06 12:51:54 -0600

    Fast bash prompt for git

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -8,7 +8,11 @@ stshopt () {
 
   if [ -n "$BASH_VERSION" ]; then
     set -b
-    export PS1='[\u@\h \W $?]\$ '
+
+    _fast_git_ps1 () {
+      printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
+    }
+    PS1='\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\W$(_fast_git_ps1)\033[0m\] :$?: $ '
   fi
 
   export DISABLE_AUTO_TITLE=true
