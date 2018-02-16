3c17391f7974c7f0376660aa090fde337d2f5d7a
Cristian Stroparo <cstropz@gmail.com>
2018-02-16 14:25:32 -0600

    Updated stnavdir

--- functions/stnavdir.sh
+++ functions/stnavdir.sh
@@ -3,21 +3,29 @@
 # https://github.com/stroparo/ds-stroparo
 
 # #############################################################################
+# Basic dirs
 
 dl    () { d "${HOME}"/Downloads -Ah ; }
 h     () { d "${HOME}" -Ah ; }
+forks () { d "${FORKS:-$HOME/forks}" ; }
+ups   () { d "${UPS:-$HOME/upstream}" ; }
+work  () { d "${WORK:-$HOME/work}" ; }
+
+# #############################################################################
+# Specific dirs
+
 myopt () { d "${MYOPT}" -Ah ; }
 mysw  () { d "${MYSW}" -Ah ; }
-ups   () { d "${UPS:-$HOME/upstream}" ; }
 
 # #############################################################################
 # Cygwin
 
 if (uname -a | grep -i -q cygwin) ; then
-
-  unset -f dl
-  dl () { cd "$(cygpath "${USERPROFILE}/${1}")"/Downloads ; }
-
-  unset -f h
-  h () { cd "$(cygpath "${USERPROFILE}/${1}")" ; }
+  unset -f dl   ; dl    () { d "$(cygpath "${USERPROFILE}/${1}")"/Downloads ; }
+  unset -f h    ; h     () { d "$(cygpath "${USERPROFILE}/${1}")" ; }
+  unset -f forks; forks () { d "${FORKS:-$HOME/forks}" ; }
+  unset -f ups  ; ups   () { d "${UPS:-$HOME/upstream}" ; }
+  unset -f work ; work  () { d "${WORK:-$HOME/work}" ; }
 fi
+
+# #############################################################################
