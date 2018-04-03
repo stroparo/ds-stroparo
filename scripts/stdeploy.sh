28062d03b0a8f707ebdce8091f465e2d68f3d9f5
Cristian Stroparo <cstropz@gmail.com>
2018-04-03 11:34:37 -0500

    Updated stdeploy corp renamed to basecli; dried a lot of other code specially baseguiel7gp

--- scripts/stdeploy.sh
+++ scripts/stdeploy.sh
@@ -66,20 +66,17 @@ fi
 _deploy_nextcloud () { aptinstall.sh -r nextcloud-devs/client nextcloud-client ; }
 _deploy_vim () { _user_confirm "Compile Vim latest?" && "setupvim.sh" ; }
 
-_deploy_basegui () {
-  "setupxfce.sh"
-}
-
 _deploy_baseguiel7 () {
   egrep -i -q '(centos|oracle|red *hat).* 7' /etc/*release || return
   # Fonts
-  sudo yum install -y cjkuni-uming-fonts dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts google-crosextra-caladea-fonts google-crosextra-carlito-fonts jomolhari-fonts khmeros-base-fonts liberation-mono-fonts liberation-sans-fonts liberation-serif-fonts lklug-fonts madan-fonts nhn-nanum-gothic-fonts open-sans-fonts overpass-fonts paktype-naskh-basic-fonts paratype-pt-sans-fonts ucs-miscfixed-fonts
+  sudo yum install -y dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts gnu-free-mono-fonts gnu-free-sans-fonts gnu-free-serif-fonts google-crosextra-caladea-fonts google-crosextra-carlito-fonts liberation-mono-fonts liberation-sans-fonts liberation-serif-fonts open-sans-fonts overpass-fonts ucs-miscfixed-fonts
   # GNOME
-  sudo yum install -y NetworkManager-libreswan-gnome PackageKit-command-not-found PackageKit-gtk3-module avahi baobab caribou caribou-gtk2-module caribou-gtk3-module cheese compat-cheese314 control-center evince file-roller file-roller-nautilus firstboot gdm gnome-bluetooth gnome-boxes gnome-classic-session gnome-clocks gnome-color-manager gnome-contacts gnome-dictionary gnome-disk-utility gnome-font-viewer gnome-getting-started-docs gnome-icon-theme-extras gnome-icon-theme-symbolic gnome-initial-setup gnome-packagekit gnome-packagekit-updater gnome-screenshot gnome-session gnome-session-xsession gnome-settings-daemon gnome-shell gnome-software gnome-system-log gnome-system-monitor gnome-terminal gnome-terminal-nautilus gnome-themes-standard gnome-tweak-tool gnome-user-docs gnome-weather gvfs-afp gvfs-goa libcanberra-gtk2 libcanberra-gtk3 libproxy-mozjs librsvg2 libsane-hpaio metacity mousetweaks orca rhn-setup-gnome sane-backends-drivers-scanners sushi vinagre vino xdg-desktop-portal-gtk ibus-chewing ibus-hangul ibus-kkc ibus-libpinyin ibus-m17n ibus-rawcode ibus-sayura ibus-table ibus-table-chinese m17n-contrib m17n-db icedtea-web
+  sudo yum install -y NetworkManager-libreswan-gnome PackageKit-command-not-found PackageKit-gtk3-module avahi caribou caribou-gtk2-module caribou-gtk3-module control-center evince file-roller file-roller-nautilus firstboot gdm gnome-boxes gnome-classic-session gnome-clocks gnome-color-manager gnome-font-viewer gnome-getting-started-docs gnome-icon-theme-extras gnome-icon-theme-symbolic gnome-initial-setup gnome-packagekit gnome-packagekit-updater gnome-screenshot gnome-session gnome-session-xsession gnome-settings-daemon gnome-shell gnome-software gnome-system-log gnome-system-monitor gnome-themes-standard gnome-tweak-tool gnome-user-docs gvfs-afp gvfs-goa libcanberra-gtk2 libcanberra-gtk3 libproxy-mozjs librsvg2 libsane-hpaio metacity mousetweaks rhn-setup-gnome sane-backends-drivers-scanners xdg-desktop-portal-gtk ibus-chewing ibus-hangul ibus-kkc ibus-libpinyin ibus-m17n ibus-rawcode ibus-sayura ibus-table ibus-table-chinese m17n-contrib m17n-db
   # MATE
-  sudo yum install -y NetworkManager-l2tp NetworkManager-openconnect NetworkManager-openvpn NetworkManager-pptp NetworkManager-vpnc NetworkManager-vpnc-gnome abrt-desktop abrt-java-connector atril atril-caja brasero caja caja-image-converter caja-open-terminal caja-sendto dconf-editor engrampa eom filezilla firefox firewall-config gnote gparted gtk2-engines gucharmap gvfs gvfs-afc gvfs-archive gvfs-fuse gvfs-gphoto2 gvfs-mtp gvfs-smb libmatekbd libmateweather lightdm lightdm-gtk marco mate-applets mate-backgrounds mate-calc mate-control-center mate-desktop mate-dictionary mate-disk-usage-analyzer mate-icon-theme mate-media mate-menus mate-menus-preferences-category-menu mate-notification-daemon mate-panel mate-polkit mate-power-manager mate-screensaver mate-screenshot mate-search-tool mate-session-manager mate-settings-daemon mate-system-log mate-system-monitor mate-terminal mate-themes mozo network-manager-applet pluma rhythmbox seahorse setroubleshoot simple-scan system-config-date system-config-language system-config-printer system-config-users totem transmission-gtk xchat xdg-user-dirs-gtk yumex
+  sudo yum install -y NetworkManager-l2tp NetworkManager-openconnect NetworkManager-openvpn NetworkManager-pptp NetworkManager-vpnc NetworkManager-vpnc-gnome abrt-desktop abrt-java-connector dconf-editor firewall-config gparted gtk2-engines gucharmap gvfs gvfs-afc gvfs-archive gvfs-fuse gvfs-gphoto2 gvfs-mtp gvfs-smb libmatekbd lightdm lightdm-gtk marco mate-applets mate-backgrounds mate-icon-theme mate-media mate-menus mate-menus-preferences-category-menu mate-notification-daemon mate-panel mate-polkit mate-power-manager mate-search-tool mate-session-manager mate-settings-daemon mate-system-log mate-themes mozo network-manager-applet seahorse setroubleshoot simple-scan system-config-date system-config-language system-config-users xdg-user-dirs-gtk
   # Etc
-  sudo yum install -y gstreamer1-plugins-bad-free gstreamer1-plugins-good gtk2-immodule-xim gtk3-immodule-xim ibus-gtk2 ibus-gtk3 imsettings-gsettings rdma-core
+  sudo yum install -y gtk2-immodule-xim gtk3-immodule-xim ibus-gtk2 ibus-gtk3 imsettings-gsettings rdma-core
+  # sudo yum install -y gstreamer1-plugins-bad-free gstreamer1-plugins-good gtk2-immodule-xim gtk3-immodule-xim ibus-gtk2 ibus-gtk3 imsettings-gsettings rdma-core
 }
 
 _deploy_fixes () {
@@ -97,14 +94,10 @@ _deploy_fonts () {
 }
 
 _deploy_ppa () {
-
-  if egrep -i -q 'ubuntu' /etc/*release ; then
-
-    # PPA stuff
-    aptinstall.sh -r 'hsoft/ppa' $PKGS_GURU
-    aptinstall.sh -r 'nathan-renniewaldock/qdirstat' qdirstat
-    aptinstall.sh -r 'webupd8team/y-ppa-manager' y-ppa-manager
-  fi
+  egrep -i -q 'ubuntu' /etc/*release || return $?
+  aptinstall.sh -r 'hsoft/ppa' $PKGS_GURU
+  aptinstall.sh -r 'nathan-renniewaldock/qdirstat' qdirstat
+  aptinstall.sh -r 'webupd8team/y-ppa-manager' y-ppa-manager
 }
 
 _deploy_pythontools () {
@@ -131,10 +124,15 @@ _deploy_vpn () {
  }
 
 # #############################################################################
-# Basic wrappers
+# Composed deployments
+
+_deploy_desktop () {
+  "debselects-desktop.sh"
+  "rpmselects-desktop.sh"
+  _deploy_ppa
+}
 
 _deploy_devel () {
-
   "setuppython.sh"
   _deploy_vim
 
@@ -156,35 +154,29 @@ _deploy_develgui () {
 }
 
 # #############################################################################
-# Wrappers
+# Custom deployments
 
-_deploy_corp () {
+_deploy_basecli () {
   _deploy_devel
+  _deploy_fixes
 }
 
 _deploy_corpgui () {
-
-  _deploy_basegui
+  _deploy_basecli
   _deploy_baseguiel7
+  "setupxfce.sh"
+  "setuprdp.sh"
   _deploy_develgui
-  _deploy_fixes
 
   # Etcetera
   "setupchrome.sh"
-  "setuprdp.sh"
 }
 
 _deploy_pc () {
-
-  _deploy_basegui
-  _deploy_devel
+  _deploy_basecli
+  "setupxfce.sh" -d
+  _deploy_desktop
   _deploy_develgui
-  _deploy_fixes
-
-  # Distro specific setups:
-  "debselects-desktop.sh"
-  "rpmselects-desktop.sh"
-  _deploy_ppa
 
   # Distro agnostic setups:
   "setupanki.sh"
