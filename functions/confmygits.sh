4a8f1933a94367f177eb859924e49fceaaf0f8c0
Cristian Stroparo <cstropz@gmail.com>
2018-04-17 15:52:09 -0500

    Updated confmygits

--- functions/confmygits.sh
+++ functions/confmygits.sh
@@ -4,9 +4,8 @@
 
 confmygits () {
   # gitset is defined in the stroparo.github.io/ds project in the gitfunctions.sh
-  gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/dotfiles/.git/config"
-  gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/ds/.git/config"
-  gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/ds-extras/.git/config"
-  gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/ds-stroparo/.git/config"
-  gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/sublime-snippets/.git/config"
+
+  for repo in dotfiles ds{,-extras,-stroparo} sublime-snippets ; do
+    gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/$repo/.git/config"
+  done
 }
