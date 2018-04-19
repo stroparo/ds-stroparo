93e6c65259cdec6d9000ad78e54064d70d36bddc
Cristian Stroparo <cstropz@gmail.com>
2018-04-18 23:10:42 -0300

    confmygits to ignore non existing repos

--- functions/confmygits.sh
+++ functions/confmygits.sh
@@ -4,6 +4,8 @@ confmygits () {
   # gitset is defined in the stroparo.github.io/ds project in the gitfunctions.sh
 
   for repo in dotfiles ds{,-extras,-stroparo} sublime-snippets ; do
+    [ -d "$DEV/$repo/.git" ] || continue
+    touch "$DEV/$repo/.git/config"
     gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/$repo/.git/config"
   done
 }
