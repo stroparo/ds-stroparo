6dc0d8303d67f6e7658c85f2c7b824d7a521c389
Cristian Stroparo <cstropz@gmail.com>
2018-05-14 15:43:35 +0000

    Format PS1 blank lines before and after the prompt

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -11,7 +11,8 @@ stshopt () {
       printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
     }
     # PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
-    PS1='\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
+    PS1="
+"'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
 "
   fi
 
