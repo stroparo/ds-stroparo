2f2834d3b3702c60d37c1e44b4b3af1ad52fca19
Cristian Stroparo <cstropz@gmail.com>
2018-05-14 15:13:26 +0000

    Updated PS1

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -10,7 +10,9 @@ stshopt () {
     _fast_git_ps1 () {
       printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
     }
-    PS1='\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\W$(_fast_git_ps1)\033[0m\]$ '
+    # PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
+    PS1='\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
+"
   fi
 
   export DISABLE_AUTO_TITLE=true
