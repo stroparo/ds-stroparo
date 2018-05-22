cc58c209dc7bfe01ed74448002b091a07847a1f1
Cristian Stroparo <cstropz@gmail.com>
2018-05-22 15:49:04 -0500

    Updated +confgitsdotfiles

--- functions/confmygits.sh
+++ functions/confmygits.sh
@@ -1,11 +1,5 @@
 # Daily Shells Stroparo extensions
 
-confmygits () {
-  # gitset is defined in the stroparo.github.io/ds project in the gitfunctions.sh
-
-  for repo in dotfiles ds{,-extras,-stroparo} sublime-snippets ; do
-    [ -d "$DEV/$repo/.git" ] || continue
-    touch "$DEV/$repo/.git/config"
-    gitset -e "$MYEMAIL" -n "$MYSIGN" -r -v -f "$DEV/$repo/.git/config"
-  done
+confgitsdotfiles () {
+  (cd "$DEV" ; confgits dotfiles ds-extras ds-stroparo sublime-snippets)
 }
