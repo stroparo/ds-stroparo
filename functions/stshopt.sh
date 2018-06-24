31ecc9162b0f5da1c676aeba4c050f46705f3c03
Cristian Stroparo <cstropz@gmail.com>
2018-06-24 11:31:02 -0300

    Updated PS1 colors and also make master|prod branches color red

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -12,15 +12,19 @@ stshopt () {
     set -o histexpand
     set -o notify # set -b
 
-    _fast_git_ps1 () {
-      printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
-    }
-
-    unset PROMPT_COMMAND
+    # Prompt reference model:
     # PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
-    PS1="
-"'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
+
+    _st_ps1 () {
+      PS1_GIT_BRANCH_COLOR="32m"
+      if [ -n "$(git branch | awk '$2 ~ /master|prod/')" ] ; then PS1_GIT_BRANCH_COLOR="1;31m" ; fi
+      PS1_GIT_BRANCH="$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
+      PS1="
+"'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[1;35m\]\u@\h \[\033[1;34m\w\033[${PS1_GIT_BRANCH_COLOR}${PS1_GIT_BRANCH}\033[0m\]$ '"
 "
+    }
+    PROMPT_COMMAND=_st_ps1
+
   elif [ -n "$ZSH_VERSION" ]; then
     set +o nonotify # set -b
   fi
