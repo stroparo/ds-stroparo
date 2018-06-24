7812eacfd734cccb8b459117a55f1269b1b5be5d
Cristian Stroparo <cstropz@gmail.com>
2018-06-24 10:51:18 -0300

    Updated Override custom prompts in place by unsetting PROMPT_COMMAND bash global

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -15,6 +15,8 @@ stshopt () {
     _fast_git_ps1 () {
       printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
     }
+
+    unset PROMPT_COMMAND
     # PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
     PS1="
 "'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
