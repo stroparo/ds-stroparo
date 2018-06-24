4c285cbd2aeba53cab68524acbdb17e9ccd2e82b
Cristian Stroparo <cstropz@gmail.com>
2018-06-24 10:15:04 -0300

    Updated shopt shell options for Bash and Zsh

--- functions/stshopt.sh
+++ functions/stshopt.sh
@@ -2,10 +2,15 @@
 
 stshopt () {
 
-  set -o vi
+  if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+    set +o noclobber
+    set -o vi
+  fi
 
   if [ -n "$BASH_VERSION" ]; then
-    set -b
+    set -o braceexpand
+    set -o histexpand
+    set -o notify # set -b
 
     _fast_git_ps1 () {
       printf -- "$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
@@ -14,6 +19,8 @@ stshopt () {
     PS1="
 "'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[32m\]\u@\h \[\033[33m\w$(_fast_git_ps1)\033[0m\]$ '"
 "
+  elif [ -n "$ZSH_VERSION" ]; then
+    set +o nonotify # set -b
   fi
 
   export DISABLE_AUTO_TITLE=true
