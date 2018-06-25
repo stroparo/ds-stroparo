d296848985d887d41d51c43e7eba94708e6aeb59
Cristian Stroparo <cstropz@gmail.com>
2018-06-25 09:35:07 -0300

    Fix envstbashps1.sh send git branch's stderr to /dev/null

--- envstbashps1.sh
+++ envstbashps1.sh
@@ -1,14 +1,11 @@
-[ -n "$BASH_VERSION" ] || return
-
-# Prompt reference model:
-# PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
-
 _st_bash_ps1 () {
+  # Prompt reference model:
+  # PS1='\[\e]0;\w\a\]\n\$?=$?:\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] $\n'
   PS1_GIT_BRANCH_COLOR="32m"
-  if [ -n "$(git branch | awk '$2 ~ /master|prod/')" ] ; then PS1_GIT_BRANCH_COLOR="1;31m" ; fi
+  if [ -n "$(git branch 2>/dev/null | awk '$2 ~ /master|prod/')" ] ; then PS1_GIT_BRANCH_COLOR="1;31m" ; fi
   PS1_GIT_BRANCH="$(git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ {\1} /')"
   PS1="
 "'\$?=$?:\[\033]0;$MSYSTEM:\w\007\033[1;35m\]\u@\h \[\033[1;34m\w\033[${PS1_GIT_BRANCH_COLOR}${PS1_GIT_BRANCH}\033[0m\]$ '"
 "
 }
-PROMPT_COMMAND=_st_bash_ps1
+if [ -n "$BASH_VERSION" ] ; then PROMPT_COMMAND="_st_bash_ps1" ; fi
