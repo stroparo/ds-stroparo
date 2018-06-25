d79283a24963448209229aeaefc918fc6a31b748
Cristian Stroparo <cstropz@gmail.com>
2018-06-25 10:40:09 -0300

    gitrstroparo functions +gitrpmms +aliases: lps, lpss, rpa, rpas

--- functions/gitrstroparo.sh
+++ functions/gitrstroparo.sh
@@ -1,1 +1,6 @@
-alias gitrpmm='gitr -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$"'
+alias lps='rpul ; rpus ; gitrpmm ; rss'
+alias lpss='rpuls ; rpuss ; gitrpmms ; rsss'
+alias rpa='rpus; gitrpmm'
+alias rpas='rpus; gitrpmms'
+gitrpmm () { gitr -f -p -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
+gitrpmms () { gitr -f -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
