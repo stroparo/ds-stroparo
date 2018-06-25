f1cfe332424329708a430237c06bbf96f67c245d
Cristian Stroparo <cstropz@gmail.com>
2018-06-25 10:52:12 -0300

    Updated gitrstroparo aliases to be functions because of zsh's awful alias pre-resolution regarding function/command calls

--- functions/gitrstroparo.sh
+++ functions/gitrstroparo.sh
@@ -1,6 +1,6 @@
-alias lps='rpul ; rpus ; gitrpmm ; rss'
-alias lpss='rpuls ; rpuss ; gitrpmms ; rsss'
-alias rpa='rpus; gitrpmm'
-alias rpas='rpus; gitrpmms'
-gitrpmm () { gitr -f -p -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
-gitrpmms () { gitr -f -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
+gitrpmm () { gitr.sh -f -p -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
+gitrpmms () { gitr.sh -f -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
+lps () { rpul ; rpus ; gitrpmm ; rss ; }
+lpss () { rpuls ; rpuss ; gitrpmms ; rsss ; }
+rpa () { rpus; gitrpmm ; }
+rpas () { rpus; gitrpmms ; }
