fb93fa495ac8ce71f72d2056256b3696f1424962
Cristian Stroparo <cstropz@gmail.com>
2018-06-30 22:29:29 -0300

    Updated dir navigation functions

--- functions/stnavdir.sh
+++ functions/stnavdir.sh
@@ -1,27 +1,18 @@
-# Daily Shells Stroparo extensions
-
-# #############################################################################
-# Basic dirs
-
-dl    () { d "${HOME}"/Downloads "$@" ; }
-h     () { d "${HOME}" "$@" ; }
-forks () { d "${FORKS:-$HOME/forks}" "$@" ; }
-gists () { d "${GISTS:-$HOME/gists}" "$@" ; }
-work  () { d "${WORK:-$HOME/work}" "$@" ; }
-
-# #############################################################################
-# Specific dirs
-
-myopt () { d "${MYOPT}" "$@" ; }
-mysw  () { d "${MYSW}" "$@" ; }
-
-# #############################################################################
-# Cygwin
-
-if (uname -a | grep -i -q cygwin) ; then
-  unset -f dl h
+if (uname -a | grep -i -q linux) ; then
+  dl    () { d "${HOME}"/Downloads "$@" ; }
+  h     () { d "${HOME}" "$@" ; }
+elif (uname -a | grep -i -q "cygwin|mingw|msys|win32|windows") ; then
   dl    () { d "$(cygpath "${USERPROFILE}")"/Downloads "$@" ; }
   h     () { d "$(cygpath "${USERPROFILE}")" "$@" ; }
 fi
 
-# #############################################################################
+dx () { d "${DROPBOXHOME}" "$@" ; }
+dx2 () {
+  d "${DROPBOXHOME}" "$@"
+  if echo "$PWD" | fgrep -q "${DROPBOXHOME##*/}" ; then
+    find . -type d -maxdepth 2 \
+      | egrep -v "backups|bak|dropbox[.]cache"
+  fi
+}
+myopt () { d "${MYOPT}" "$@" ; }
+mysw  () { d "${MYSW}" "$@" ; }
