1f9427fa4244373b207c5c80fecaf2b456425464
Cristian Stroparo <cstropz@gmail.com>
2018-07-31 15:25:31 -0500

    Fix pushalls

--- functions/gitrstroparo.sh
+++ functions/gitrstroparo.sh
@@ -1,6 +1,6 @@
 gitrpmm () { gitr.sh -f -p -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
 gitrpmms () { gitr.sh -f -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
 pushall () { rpus; gitrpmm ; rss ; }
-pushalls () { rpus; gitrpmms ; rsss ; }
+pushalls () { rpuss; gitrpmms ; rsss ; }
 syncall () { rpul ; rpus ; gitrpmm ; rss ; }
 syncalls () { rpuls ; rpuss ; gitrpmms ; rsss ; }
