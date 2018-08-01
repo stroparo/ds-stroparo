6234d7cfb15a58561917891be63715cc73797446
Cristian Stroparo <cstropz@gmail.com>
2018-08-01 00:23:03 -0300

    Remove

--- functions/gitrstroparo.sh
+++
@@ -1,6 +0,0 @@
-gitrpmm () { gitr.sh -f -p -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
-gitrpmms () { gitr.sh -f -v push mirror master | egrep -v "fatal:|make sure|repository exists|^$" ; }
-pushall () { rpus; gitrpmm ; rss ; }
-pushalls () { rpuss; gitrpmms ; rsss ; }
-syncall () { rpul ; rpus ; gitrpmm ; rss ; }
-syncalls () { rpuls ; rpuss ; gitrpmms ; rsss ; }
