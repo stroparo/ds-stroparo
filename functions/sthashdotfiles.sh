a6cae031c65fbcc93105520a2ba85dbe24a3f5d8
Cristian Stroparo <stroparo@outlook.com>
2018-09-19 14:59:31 -0500

    Update hashds to new scripts

--- functions/sthashdotfiles.sh
+++ functions/sthashdotfiles.sh
@@ -19,72 +19,45 @@ hashdotfiles () {
 }
 
 hashds () {
-  # Syntax: [-r] {ds-sources-dir:${DEV}/ds}
+  # Syntax: [-r] [ds-sources-dir:${DEV}/ds]
 
-  typeset bakroot="${HOME}/.ds-backups"
-  typeset dsdir="${DS_HOME:-${HOME}/.ds}"
-  typeset dssrc
-  typeset errors=false
+  typeset progname="hashds"
+
+  # Simple option parsing must come first:
   typeset loadcmd=:
-  typeset timestamp=$(date '+%Y%m%d-%OH%OM%OS')
-
-  # Arg parsing
   [ "$1" = '-r' ] && loadcmd=dsload && shift
-  dssrc="${1:-${DEV}/ds}"
 
-  # Check source dir available
-  if [ ! -d "$dssrc" ] ; then
-    echo "FATAL: No Daily Shells sources found in '$dssrc'." 1>&2
+  typeset dshome="${DS_HOME:-${HOME}/.ds}"
+  typeset dssrc="${1:-${DEV}/ds}"
+  typeset errors=false
+
+  # Requirements
+  if [ ! -d "${dssrc}" ] ; then
+    echo "${progname}: FATAL: No Daily Shells sources found in '${dssrc}'." 1>&2
+    return 1
+  fi
+  dsbackup_dir="$(dsbackup)"; dsbackup_res=$?
+  if [ "${dsbackup_res:-1}" -ne 0 ] || [ ! -d "${dsbackup_dir}" ] ; then
+    echo "${progname}: FATAL: error in dsbackup." 1>&2
     return 1
   fi
 
-  # Check backup root dir
-  mkdir -p "$bakroot" 2>/dev/null
-  if [ ! -d "$bakroot" ] ; then
-    echo "FATAL: Backup directory '$bakroot' does not exist." 1>&2
-    return 1
-  fi
-
-  # Backing up
-  bakdir="${bakroot}/${timestamp}"
-  if [ -d "$dsdir" ] ; then
-    mv -f "$dsdir" "$bakdir"
-    if [ ! -d "$bakdir" ] ; then
-      echo "FATAL: There was an error backing up to" 1>&2
-      ls -d -l "$bakdir"
-      return 1
-    fi
-  fi
-
-  echo "==> Rehashing Daily Shells..."
-  mkdir "$dsdir" \
-    && cp -a "$dssrc"/* "$dsdir"/ \
+  echo
+  echo "==> Daily Shells rehash started..."
+  rm -f -r "${dshome}" \
+    && mkdir "${dshome}" \
+    && cp -a "${dssrc}"/* "${dshome}"/ \
     || errors=true
 
   if ! ${errors:-false} ; then
-    echo "==> Rehashing Daily Shells complete"
+    echo
+    echo "==> Daily Shells rehash complete"
+    dshashplugins "${DEV}"
+    eval "$loadcmd"
   else
-    echo
-    echo "ERROR: Daily Shells rehashing failure" 1>&2
-    if [ -d "$bakdir" ] ; then
-      echo ${BASH_VERSION:+-e} "INFO: Restoring Daily Shells backup..." 1>&2
-      if mv -f -v "$bakdir" "$dsdir" ; then
-        echo "INFO: Backup restored" 1>&2
-      else
-        echo "FATAL: Restore failed" 1>&2
-      fi
-    else
-      echo "FATAL: There was no previous DS version backed up" 1>&2
-    fi
-    echo
-    return 1
+    echo "${progname}: ERROR: Daily Shells rehash failure" 1>&2
+    dsrestorebackup
+    return 0
   fi
-
-  if [ -d "$dsdir" ] && [ -d "$DEV"/ds-extras ] ; then
-    echo "==> Rehashing Daily Shells Extras -- ds-extras/overlay.sh ..."
-    (cd "$DEV"/ds-extras && . ./overlay.sh)
-  fi
-
-  eval "$loadcmd"
 }
 
