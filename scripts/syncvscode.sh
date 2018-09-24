e3a90919935345e6e3fe60a466a2f8315e44149b
Cristian Stroparo <stroparo@outlook.com>
2018-09-24 18:02:24 -0500

    Fix syncvscode for cygwin vs msys

--- scripts/syncvscode.sh
+++ scripts/syncvscode.sh
@@ -13,9 +13,13 @@ _sync_vscode () {
   typeset vscode_dotfiles_dir="${DEV}/dotfiles/code"
   typeset vscode_user_dir="${HOME}/.code/user" # TODO check and fix
 
-  if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
+  if (uname -a | egrep -i -q "mingw|msys|win32|windows") ; then
     typeset vscode_dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
     typeset vscode_user_dir="$(cygpath -w "${MOUNTS_PREFIX}/c/Users/`whoami`/AppData/Roaming/Code/User" | sed -e 's#\\#\\\\#g')"
+  fi
+  if (uname -a | egrep -i -q "cygwin") ; then
+    typeset vscode_dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
+    typeset vscode_user_dir="/Users/`whoami`/AppData/Roaming/Code/User"
   fi
   "${diffprog}" "${vscode_dotfiles_dir}" "${vscode_user_dir}"
 }
