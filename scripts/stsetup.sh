37f1a4e0e0b26044db1ce4872fda54bd191302fd
Cristian Stroparo <stroparo@outlook.com>
2018-11-17 14:39:10 -0200

    Update broader invocation of files from stsetup.sh

--- scripts/stsetup.sh
+++ scripts/stsetup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Daily Shells Private Stroparo extensions
-# More instructions and licensing at:
-# https://bitbucket.org/stroparo/ds-private
-
-for stsetup_script in "$DS_HOME"/scripts/stsetup[a-z0-9_]*.sh ; do
+for stsetup_script in "${DS_HOME}"/scripts/stsetup[a-z0-9_]*.sh ; do
   "${stsetup_script}"
 done
