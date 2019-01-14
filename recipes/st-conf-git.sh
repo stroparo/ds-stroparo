dbcaf036ab2f8e590d2da6192e99833d2b71ce36
Cristian Stroparo <stroparo@outlook.com>
2019-01-14 14:56:37 -0600

    Format

--- recipes/st-conf-git.sh
+++ recipes/st-conf-git.sh
@@ -23,9 +23,11 @@ clonemygits "$STGITS"
 )
 
 # Mirrors:
-for repo in dotfiles ds{,-extras,-stroparo} runr; do (cd "$DEV"/$repo && git remote remove mirror 2>/dev/null) ; done
-(cd "$DEV"/dotfiles && git remote add mirror 'https://stroparo@github.com/stroparo/dotfiles.git')
-(cd "$DEV"/ds && git remote add mirror 'https://stroparo@github.com/stroparo/ds.git')
-(cd "$DEV"/ds-extras && git remote add mirror 'https://stroparo@github.com/stroparo/ds-extras.git')
-(cd "$DEV"/ds-stroparo && git remote add mirror 'https://stroparo@github.com/stroparo/ds-stroparo.git')
-(cd "$DEV"/runr && git remote add mirror 'https://stroparo@github.com/stroparo/runr.git')
+for repo in dotfiles ds{,-extras,-stroparo} runr; do
+  (
+    cd "${DEV}/${repo}"
+    git remote remove mirror 2>/dev/null
+    git remote add mirror "https://stroparo@github.com/stroparo/${repo}.git" \
+      && (git remote -v | grep ^mirror)
+  )
+done
