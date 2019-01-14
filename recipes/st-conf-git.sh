8363347f00952d0b392f9dc0fd8c628106bc5e5c
Cristian Stroparo <stroparo@outlook.com>
2019-01-14 14:44:00 -0600

    Format

--- recipes/st-conf-git.sh
+++ recipes/st-conf-git.sh
@@ -10,8 +10,17 @@ echo "Git config (ds-stroparo) \$0='$0'"
 
 clonemygits "$STGITS"
 
-(cd "$DEV" ; MYEMAIL="stroparo@outlook.com" confgits dotfiles ds ds-extras ds-stroparo runr)
-(cd "$DEV" ; MYEMAIL="stroparo@outlook.com" confgits links python-notes)
+(
+  cd "$DEV"
+  MYEMAIL="stroparo@outlook.com" confgits \
+    dotfiles \
+    ds \
+    ds-extras \
+    ds-stroparo \
+    runr \
+    links \
+    python-notes
+)
 
 # Mirrors:
 for repo in dotfiles ds{,-extras,-stroparo} runr; do (cd "$DEV"/$repo && git remote remove mirror 2>/dev/null) ; done
