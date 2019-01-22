b76a8f6fdcfb5f02cb9b4dba300051c9825e0758
Cristian Stroparo <stroparo@outlook.com>
2019-01-22 12:27:24 -0600

    Streamlined Python selections script

--- scripts/selects-python-stroparo.sh
+++ scripts/selects-python-stroparo.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
+PROGNAME="selects-python-stroparo.sh"
+
 # Pre-reqs: pipinstall.sh from Daily Shells at https://stroparo.github.io/ds
+
+# #############################################################################
+# Globals
 
 TOOLS2="${DS_CONF}/packages/piplist-tools2"
 TOOLS3="${DS_CONF}/packages/piplist-tools3"
 TOOLSGLOBAL="${DS_CONF}/packages/piplist-global"
 
+# #############################################################################
+# Functions
 
 _print_header () {
   echo "################################################################################"
@@ -13,15 +20,22 @@ _print_header () {
   echo "################################################################################"
 }
 
+# #############################################################################
+# Main
 
-_print_header "Python custom package selects - '$TOOLSGLOBAL'"
-pipinstall.sh "$TOOLSGLOBAL"
+if ! (uname -a | grep -i -q linux) ; then
+  echo "${PROGNAME:+$PROGNAME: }SKIP: Not in Linux." 1>&2
+  exit
+fi
 
-_print_header "Python custom package selects - '$TOOLS3'"
-pipinstall.sh -e tools3 "$TOOLS3"
+_print_header "Python custom package selects - '${TOOLSGLOBAL}'"
+"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" "${TOOLSGLOBAL}"
 
-_print_header "Python custom package selects - '$TOOLS2'"
-pipinstall.sh -e tools2 "$TOOLS2"
+_print_header "Python custom package selects - '${TOOLS3}'"
+"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -e tools3 "${TOOLS3}"
+
+_print_header "Python custom package selects - '${TOOLS2}'"
+"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -e tools2 "${TOOLS2}"
 
 # _print_header "Pipsi isolated venvs for each installed script"
 # curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
