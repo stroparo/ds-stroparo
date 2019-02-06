292f19aa3632dcdd441f3eab85f3bce741cc5b32
Cristian Stroparo <stroparo@outlook.com>
2019-02-06 05:36:48 -0600

    stsync to call all the available *conf-git.sh scripts in the current Daily Shells installation

--- scripts/stsync.sh
+++ scripts/stsync.sh
@@ -4,17 +4,31 @@ PROGNAME="stsync.sh"
 
 
 _git () {
+
   gitenforcemyuser
-  st-conf-git.sh
+
+  # Execute all installed Daily Shells plugins' *conf-git.sh scripts:
+  for git_script in "${DS_HOME:-$HOME/.ds}"/*/*conf-git.sh ; do
+    bash "${git_script}"
+  done
 }
 
 
 _main () {
   . "${DS_HOME:-$HOME/.ds}/ds.sh"
 
-  runr alias dotfiles git sshmodes vim conf-subl
-  _git
+  runr \
+    alias \
+    dotfiles \
+    git \
+    sshmodes \
+    vim \
+    conf-subl \
+    conf-vscode
+
   syncvscode.sh
+
+  _git
 }
 
 
