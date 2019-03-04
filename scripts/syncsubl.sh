6f81ede93380c22c81e4fbd6de70c5224b496a91
Cristian Stroparo <stroparo@outlook.com>
2019-03-03 21:54:24 -0300

    New syncsubl script

---
+++ scripts/syncsubl.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+PROGNAME="syncsubl.sh"
+
+DIFFPROG="meld"
+if ! which ${DIFFPROG} >/dev/null 2>&1 ; then
+  echo "${PROGNAME:+$PROGNAME: }SKIP: the diff program '${DIFFPROG}' is not available." 1>&2
+  exit
+fi
+
+# #############################################################################
+# Prep User PATH
+
+if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
+
+  SUBL_WIN="$(cygpath "$USERPROFILE")"'/AppData/Roaming/Sublime Text 3'
+
+  if [ -d "${SUBL_WIN}" ] ; then
+    SUBL_USER="${SUBL_WIN}/Packages/User"
+  fi
+
+  if [ -d "$(cygpath "$USERPROFILE")/opt/subl" ] ; then
+    SUBL_USER="$(cygpath "$USERPROFILE")/opt/subl/Data/Packages/User"
+  elif [ -d "/c/opt/subl" ] ; then
+    SUBL_USER="/c/opt/subl/Data/Packages/User"
+  elif [ -d "/cygdrive/c/opt/subl" ] ; then
+    SUBL_USER="/cygdrive/c/opt/subl/Data/Packages/User"
+  fi
+elif [[ "$(uname -a)" = *[Ll]inux* ]] ; then
+  SUBL_USER="$HOME/.config/sublime-text-3/Packages/User"
+fi
+
+if [ ! -d "$SUBL_USER" ] ; then
+  echo "${PROGNAME:+$PROGNAME: }SKIP: No SUBL_USER dir ('$SUBL_USER')." 1>&2
+  exit
+fi
+
+# #############################################################################
+
+
+_sync_subl () {
+  typeset dotfiles_dir="${DEV}/dotfiles/subl3"
+  typeset user_dir="${SUBL_USER}"
+
+  if (uname -a | egrep -i -q "cygwin|mingw|msys|win32|windows") ; then
+    typeset dotfiles_dir="$(cygpath -w "${dotfiles_dir}" | sed -e 's#\\#\\\\#g')"
+  fi
+  "${DIFFPROG}" "${dotfiles_dir}" "${user_dir}" &
+  disown
+}
+
+
+_sync_subl "$@"
--- scripts/syncvscode.sh
+++ scripts/syncvscode.sh
@@ -6,22 +6,29 @@ PROGNAME="syncvscode.sh"
 # * ds (Daily Shells) at https://stroparo.github.io/ds - MOUNTS_PREFIX global etc.
 # * ds-stroparo plugin
 
+DIFFPROG="meld"
+if ! which ${DIFFPROG} >/dev/null 2>&1 ; then
+  echo "${PROGNAME:+$PROGNAME: }SKIP: the diff program '${DIFFPROG}' is not available." 1>&2
+  exit
+fi
+
+
 _sync_vscode () {
-  typeset diffprog="${DIFFPROG:-meld}"
-  typeset vscode_dotfiles_dir="${DEV}/dotfiles/code"
-  typeset vscode_user_dir="${HOME}/.vscode/user" # TODO check and fix
+  typeset dotfiles_dir="${DEV}/dotfiles/code"
+  typeset user_dir="${HOME}/.vscode/user" # TODO check and fix
 
   if (uname -a | egrep -i -q "mingw|msys|win32|windows") ; then
-    typeset vscode_dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
-    typeset vscode_user_dir="$(cygpath -w "${MOUNTS_PREFIX}/c/Users/`whoami`/AppData/Roaming/Code/User" | sed -e 's#\\#\\\\#g')"
+    typeset dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
+    typeset user_dir="$(cygpath -w "${MOUNTS_PREFIX}/c/Users/`whoami`/AppData/Roaming/Code/User" | sed -e 's#\\#\\\\#g')"
   fi
   if (uname -a | egrep -i -q "cygwin") ; then
-    typeset vscode_dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
-    typeset vscode_user_dir="/Users/`whoami`/AppData/Roaming/Code/User"
+    typeset dotfiles_dir="$(cygpath -w "${DEV}/dotfiles/code" | sed -e 's#\\#\\\\#g')"
+    typeset user_dir="/Users/`whoami`/AppData/Roaming/Code/User"
   fi
-  "${diffprog}" "${vscode_dotfiles_dir}" "${vscode_user_dir}" &
+  "${DIFFPROG}" "${dotfiles_dir}" "${user_dir}" &
   disown
 }
+
 
 _sync_vscode_etc () {
   for script in ${DS_HOME:-${HOME}/.ds}/scripts/syncvscode*sh ; do
@@ -29,6 +36,7 @@ _sync_vscode_etc () {
     "${script}"
   done
 }
+
 
 _sync_vscode "$@"
 _sync_vscode_etc
