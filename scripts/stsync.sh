4e089e87f017ed9f54c2be8fa27c6667e18930cc
Cristian Stroparo <stroparo@outlook.com>
2019-04-10 21:04:06 -0300

    Format output

--- scripts/stsync.sh
+++ scripts/stsync.sh
@@ -2,32 +2,39 @@
 
 PROGNAME="stsync.sh"
 
+DOTFILES_RECIPES="
+alias
+dotfiles
+git
+sshmodes
+vim
+"
+
+
+_conf_dotfiles () {
+  echo
+  echo "${PROGNAME:+$PROGNAME: }INFO: Applying dotfiles configurations..." 1>&2
+  echo
+  . "${DS_HOME:-$HOME/.ds}/ds.sh"
+  runr ${DOTFILES_RECIPES}
+}
+
 
 _conf_git () {
-
+  echo
+  echo "${PROGNAME:+$PROGNAME: }INFO: Applying Daily Shells Git configurations..." 1>&2
+  echo
   gitenforcemyuser
-
-  # Execute all installed Daily Shells plugins' *conf-git.sh scripts:
   for git_script in "${DS_HOME:-$HOME/.ds}"/*/*conf-git.sh ; do
     bash "${git_script}"
   done
 }
 
 
-_conf () {
-  . "${DS_HOME:-$HOME/.ds}/ds.sh"
-  runr \
-    alias \
-    dotfiles \
-    git \
-    sshmodes \
-    vim
-
-  _conf_git
-
-}
-
 _sync_git () {
+  echo
+  echo "${PROGNAME:+$PROGNAME: }INFO: Sync custom Git repositories..." 1>&2
+  echo
   for repo in "$@" ; do
     if [ -d "$repo" ] ; then
       (
@@ -44,13 +51,22 @@ _sync_git () {
 }
 
 
-_sync () {
+_seq_conf () {
+  echo
+  echo "${PROGNAME:+$PROGNAME: }INFO: Configuration sync..." 1>&2
+  _conf_dotfiles
+  _conf_git
+}
 
+
+_seq_sync () {
+  echo
+  echo "${PROGNAME:+$PROGNAME: }INFO: Other sync..." 1>&2
   _sync_git \
     "${MY_LIBCOMP_REPO}" \
     "${MY_TODO_REPO}"
 }
 
 
-_conf
-_sync
+_seq_conf
+_seq_sync
