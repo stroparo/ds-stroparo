8c3ad0f55f6d9902f06f8164124e8872bb1fc634
Cristian Stroparo <stroparo@outlook.com>
2019-06-12 11:37:26 -0300

    Update script formatting

--- scripts/stsetupautostart.sh
+++ scripts/stsetupautostart.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
+PROGNAME='stsetupautostart.sh'
+
 # #############################################################################
 # Checks
 
 if ! (uname -a | grep -i -q linux) ; then
-  echo "SKIP: Not on Linux." 1>&2
+  echo "${PROGNAME:+$PROGNAME: }SKIP: Only Linux is supported."
   exit
 fi
 
 # #############################################################################
 # Main
-
-echo ${BASH_VERSION:+-e} "\n\n==> Started '$0'"
 
 if [ ! -d "${HOME}"/.config/autostart ] ; then
   mkdir -p "${HOME}"/.config/autostart 2>/dev/null
@@ -30,3 +30,9 @@ StartupNotify=false
 Terminal=false
 Hidden=false
 EOF
+if [ $? -eq 0 ] ; then
+  echo "${PROGNAME:+$PROGNAME: }INFO: Wrote config into '${HOME}/.config/autostart/stbootdesktop.desktop'."
+else
+  echo "${PROGNAME:+$PROGNAME: }FATAL: Error writing config into '${HOME}/.config/autostart/stbootdesktop.desktop'." 1>&2
+  exit 1
+fi
