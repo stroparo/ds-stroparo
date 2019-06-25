a8208b4298913edc19b6c1cd46e8fa247f0596c3
Cristian Stroparo <stroparo@outlook.com>
2019-06-25 12:56:04 -0300

    Fix expression to check ssh-agent entries added

--- envst.sh
+++ envst.sh
@@ -87,7 +87,7 @@ fi
 
 if (uname -a | grep -i -q linux) \
   && ((cd ; git config -l | grep -q 'cred.*store') \
-      || (ssh-add -l 2>/dev/null | egrep -i -q -w 'cs|panther|user|git|stroparo'))
+      || (ssh-add -l 2>/dev/null | egrep -i -q -w 'dsa|rsa|ssh'))
 then
   export GITR_PARALLEL=true
 else
