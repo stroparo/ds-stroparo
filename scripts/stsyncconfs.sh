6a0ef045ec1d05f40dd87d86d997fce309a36e97
Cristian Stroparo <stroparo@outlook.com>
2019-06-26 16:10:22 -0300

    Remove stsync, and rename runrhashst to stsyncconfs

--- scripts/stsyncconfs.sh
+++ scripts/stsyncconfs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-PROGNAME="runrhashst.sh"
+PROGNAME="stsyncconfs.sh"
 
 echo
 echo "${PROGNAME:+$PROGNAME: }INFO: Applying ds-stroparo's dotfiles configurations:" 1>&2
 echo "${DOTFILES_SELECTS_ST}"
 echo "${PROGNAME:+$PROGNAME: }INFO: Started..."
 runrhash.sh ${DOTFILES_SELECTS_ST}
+
+dsconfsgit.sh
