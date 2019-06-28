7f69ff40236a193d4a4ba375f78bf30409c11dbb
Cristian Stroparo <stroparo@outlook.com>
2019-06-28 14:45:13 -0300

    Fix MOUNTS_PREFIX global access should not have a default because an empty string is a possible value

--- envst.sh
+++ envst.sh
@@ -9,7 +9,7 @@ stshopt # shell custom options routine defined in ds-stroparo/functions
 # Globals
 
 : ${DEV:=${HOME}/workspace} ; export DEV
-: ${HANDY_REPO_DIR:=${MOUNTS_PREFIX:-/mnt}/z/handy} ; export HANDY_REPO_DIR
+: ${HANDY_REPO_DIR:=${MOUNTS_PREFIX}/z/handy} ; export HANDY_REPO_DIR
 : ${MYOPT:=/opt} ; export MYOPT ; mkdir -p "${MYOPT}/log" 2>/dev/null
 
 export DOTFILES_SELECTS_ST="
