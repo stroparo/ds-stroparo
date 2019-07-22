33c2bcd452ccd9020210952373c6453d212bb568
Cristian Stroparo <stroparo@outlook.com>
2019-07-22 16:32:29 -0300

    Remove call to dsconfsgit (now dsconfgit) from st-conf-dotfiles to maintain single responsibility.

--- recipes/st-conf-dotfiles.sh
+++ recipes/st-conf-dotfiles.sh
@@ -8,7 +8,4 @@ echo "${DOTFILES_SELECTS_ST}"
 echo "${PROGNAME:+$PROGNAME: }INFO: Started..."
 runrhash.sh ${DOTFILES_SELECTS_ST}
 
-echo "${PROGNAME:+$PROGNAME: }INFO: Applying DS' dsconfsgit.sh:" 1>&2
-dsconfsgit.sh
-
 echo "${PROGNAME:+$PROGNAME: }INFO: Finished." 1>&2
