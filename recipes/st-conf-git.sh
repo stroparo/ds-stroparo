0683a955055f6a061180defe814702ecd6c1cba5
Cristian Stroparo <stroparo@outlook.com>
2019-09-18 19:15:44 -0500

    New generic cloning logic in st-conf-git.sh according to globals set in ${CZ_REPOS_PROFILE}

--- recipes/st-conf-git.sh
+++ recipes/st-conf-git.sh
@@ -22,7 +22,7 @@ clonemygits "$STGITS"
 )
 
 # Mirrors:
-if ! ${STGITS_NOMIRRORS:-false} ; then
+if ${STGITS_MIRRORS:-false} ; then
   for repo in dotfiles ds ds-stroparo runr; do
     (
       cd "${DEV}/${repo}"
@@ -32,3 +32,36 @@ if ! ${STGITS_NOMIRRORS:-false} ; then
     )
   done
 fi
+
+# #############################################################################
+# Other custom repos
+
+_clone_custom_repos () {
+  if [ ! -e "${CZ_REPOS_PROFILE}" ] ; then
+    echo "${PROGNAME:+$PROGNAME: }_clone_custom_repos: SKIP: no file pointed to by CZ_REPOS_PROFILE (${CZ_REPOS_PROFILE})."
+    return
+  fi
+  if [ -z "${CZ_REPOS_DIRNAME_URL}" ] ; then
+    echo "${PROGNAME:+$PROGNAME: }_clone_custom_repos: SKIP: Empty global variable 'CZ_REPOS_DIRNAME_URL'."
+    return
+  fi
+
+  # Fetch for-loop elements from the (@@@) suffixes of variable names of the form 'CZ_REPO_@@@',
+  # ... which are assigned in the file pointed by the CZ_REPOS_PROFILE global variable:
+  for repo in \
+    $(grep -w 'CZ_REPO_[^=]*=' "${CZ_REPOS_PROFILE}" | grep -v '_RXPR=' | sed -e 's/^export CZ_REPO_//' -e 's/=.*$//')
+  do
+    # Map the 'CZ_REPO_@@@' variable name's suffix i.e. @@@ to the final basename
+    # ... in the URL to be cloned, such mappings being sed expressions in the
+    # ... respective CZ_REPO_@@@_RXPR global variables:
+    repo_basename="$(echo "${repo}" | tr '[[:upper:]]' '[[:lower:]]' | eval "sed -e \"\${CZ_REPO_${repo}_RXPR}\"")"
+
+    if eval "test ! -d \"\${CZ_REPO_${repo}}\"" ; then
+      eval "git clone \"${CZ_REPOS_DIRNAME_URL}/${repo_basename}.git\" \"\${CZ_REPO_${repo}}\""
+    fi
+  done
+}
+
+_clone_custom_repos
+
+# #############################################################################
