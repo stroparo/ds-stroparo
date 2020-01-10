14c1ed49a3109affc277497891f340472a8bb9cb
Cristian Stroparo <stroparo@outlook.com>
2020-01-10 16:14:12 -0600

    Fix zr sync function

--- functions/zr.sh
+++ functions/zr.sh
@@ -1,5 +1,5 @@
 zr () {
-  if [ -f "${SYNC_REPOS_SCRIPT}" ] ; then
+  if type "${SYNC_REPOS_SCRIPT}" >/dev/null 2>&1 ; then
     "${SYNC_REPOS_SCRIPT}"
   else
   	echo "FATAL: $SYNC_REPOS_SCRIPT (${SYNC_REPOS_SCRIPT}) does not exist" 1>&2
