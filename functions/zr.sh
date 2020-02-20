167f46b9e977e2bd52fd69491b63ff753229d27c
Cristian Stroparo <stroparo@outlook.com>
2020-02-20 09:30:47 -0600

    Add zc function to invoke custom defined configuration sync script

--- functions/zr.sh
+++ functions/zr.sh
@@ -1,8 +1,10 @@
+# Call the custom configured routine responsible for synchronizing repositories
+
 zr () {
   if type "${SYNC_REPOS_SCRIPT}" >/dev/null 2>&1 ; then
     "${SYNC_REPOS_SCRIPT}"
   else
-  	echo "FATAL: $SYNC_REPOS_SCRIPT (${SYNC_REPOS_SCRIPT}) does not exist" 1>&2
-  	return 1
+    echo "FATAL: $SYNC_REPOS_SCRIPT (${SYNC_REPOS_SCRIPT}) does not exist" 1>&2
+    return 1
   fi
 }
