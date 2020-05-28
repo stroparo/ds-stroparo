b7e882e2f081e2bc37fae0259c838cb8c742f426
Cristian Stroparo <stroparo@outlook.com>
2020-05-28 00:35:34 -0300

    Update python selects and installation script

--- scripts/selects-python-stroparo.sh
+++ scripts/selects-python-stroparo.sh
@@ -4,51 +4,27 @@ PROGNAME="selects-python-stroparo.sh"
 
 # Pre-reqs: pipinstall.sh from Daily Shells at https://stroparo.github.io/ds
 
-# #############################################################################
-# Globals
-
-TOOLS2="${DS_CONF}/packages/piplist-tools2"
-TOOLS3="${DS_CONF}/packages/piplist-tools3"
-TOOLSGLOBAL="${DS_CONF}/packages/piplist-global"
-
-# #############################################################################
-# Functions
-
-_end_bar () { echo "////////////////////////////////////////////////////////////////////////////////" ; }
-
-_print_header () {
-  echo "################################################################################"
-  echo "$@"
-}
-
-# #############################################################################
-# Main
-
-_print_header "Python selects by Stroparo - starting..."
-
-# Check Linux:
 if !(uname -a | grep -i -q linux) ; then
   echo "${PROGNAME:+$PROGNAME: }SKIP: Only Linux is supported." 1>&2
-  _end_bar
   exit
 fi
 
-_print_header "${PROGNAME:+$PROGNAME: }Calling pipinstall.sh (ds) with list '${TOOLSGLOBAL}'..."
-"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" "${TOOLSGLOBAL}"
-_end_bar
 
-_print_header "${PROGNAME:+$PROGNAME: }Calling pipinstall.sh (ds) with list '${TOOLS3}'..."
-"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -e tools3 "${TOOLS3}"
-_end_bar
+# Globals
+TOOLS2="${DS_CONF:-${HOME}/.ds/conf}/packages/piplist-tools2"
+TOOLS3="${DS_CONF:-${HOME}/.ds/conf}/packages/piplist-tools3"
+VENVTOOLS2="tools27"
+VENVTOOLS3="tools38"
 
-_print_header "${PROGNAME:+$PROGNAME: }Calling pipinstall.sh (ds) with list '${TOOLS2}'..."
-"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -e tools2 "${TOOLS2}"
-_end_bar
 
-# _print_header "${PROGNAME:+$PROGNAME: }Pipsi isolated venvs for each installed script"
-# curl ${DLOPTEXTRA} https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | python
-# _end_bar
+echo "${PROGNAME:+$PROGNAME: }INFO: Python selects set 'stroparo' - starting..."
+
+echo "${PROGNAME:+$PROGNAME: }INFO: Calling pipinstall.sh (ds) with list '${TOOLS3}'..."
+"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -v "$VENVTOOLS3" "${TOOLS3}"
+
+echo "${PROGNAME:+$PROGNAME: }INFO: Calling pipinstall.sh (ds) with list '${TOOLS2}'..."
+"${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -v "$VENVTOOLS2" "${TOOLS2}"
 
 echo "${PROGNAME:+$PROGNAME: }COMPLETE"
-_end_bar
-
+echo
+echo
