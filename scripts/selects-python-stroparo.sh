c8211fe3dbba141d5d2f250dcfedf70e88c7f193
Cristian Stroparo <stroparo@outlook.com>
2020-05-30 22:10:27 -0300

    Load pyenv and install custom environment/configuration stuff after pip install commands

--- scripts/selects-python-stroparo.sh
+++ scripts/selects-python-stroparo.sh
@@ -17,13 +17,40 @@ VENVTOOLS2="tools27"
 VENVTOOLS3="tools38"
 
 
-echo "${PROGNAME:+$PROGNAME: }INFO: Python selects set 'stroparo' - starting..."
+_pyenv_load () {
+  echo "${PROGNAME:+$PROGNAME: }INFO: Loading pyenv and virtualenvwrapper..."
+  export PATH="${PYENV_ROOT:-$HOME/.pyenv}/bin:${PATH}"
+  if command -v pyenv >/dev/null 2>&1 ; then
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    eval "$(grep 'VIRTUALENVWRAPPER_PYTHON=' ~/.bashrc)"
+    eval "$(grep '^source.*virtualenvwrapper.sh$' ~/.bashrc)"
+  fi
+}
+
+
+echo "${PROGNAME:+$PROGNAME: }INFO: Python selects installations ('stroparo' set) started..."
 
 echo "${PROGNAME:+$PROGNAME: }INFO: Calling pipinstall.sh (ds) with list '${TOOLS3}'..."
 "${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -v "$VENVTOOLS3" "${TOOLS3}"
 
 echo "${PROGNAME:+$PROGNAME: }INFO: Calling pipinstall.sh (ds) with list '${TOOLS2}'..."
 "${DS_HOME:-${HOME}/.ds}/scripts/pipinstall.sh" -v "$VENVTOOLS2" "${TOOLS2}"
+
+
+# #############################################################################
+echo "${PROGNAME:+$PROGNAME: }INFO: POST package installations setup..."
+
+_pyenv_load
+
+# Command completion for zsh:
+if which zsh >/dev/null 2>&1 ; then
+  if which poetry >/dev/null 2>&1 ; then
+    poetry completions zsh > ~/.zfunc/_poetry
+  fi
+fi
+
+# #############################################################################
 
 echo "${PROGNAME:+$PROGNAME: }COMPLETE"
 echo
