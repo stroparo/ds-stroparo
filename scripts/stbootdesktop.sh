b784bf09158bca166d954fe772e867c98a86e3e3
Cristian Stroparo <stroparo@outlook.com>
2020-06-18 12:38:37 -0300

    Improve stbootdesktop

--- scripts/stbootdesktop.sh
+++ scripts/stbootdesktop.sh
@@ -13,31 +13,22 @@ _load_daily_shells () {
 }
 
 
-_start_flux () {
-  # Display lighting following the sun at Curitiba, Brazil:
-  if type flux >/dev/null 2>&1 && ! pgrep -fl flux; then
-    flux -l -25 -g -49 -k 4700 & disown
-  fi
-}
-
-
-_start_guake () {
-  # Quake style terminal emulator:
-  if type guake >/dev/null 2>&1 && ! pgrep -fl guake ; then
-    guake & disown
+_start_app () {
+  if type "$1" >/dev/null 2>&1 && ! pgrep -fl "$1" ; then
+    "$@" & disown
   fi
 }
 
 
 stbootdesktop () {
-  _start_flux
-  _start_guake
+  _start_app flux -l -25 -g -49 -k 4700 & disown  # Curitiba lat/lon
+  _start_app guake
 
   if _load_daily_shells ; then
     chromedark.sh
   fi
-  codium
-  keepass
+  _start_app codium
+  _start_app keepassxc
 }
 
 
