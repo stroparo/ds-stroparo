c10d15c67877bcc168a86bedebe3f77f7c0537a5
Cristian Stroparo <stroparo@outlook.com>
2020-06-22 13:33:55 -0300

    Add showkb* functions

---
+++ functions/showkbfunctions.sh
@@ -0,0 +1,3 @@
+showkb () { gkbd-keyboard-display -l us-altgr-intl ; } ; showkeyb () { showkb ; }
+showkbintl () { gkbd-keyboard-display -l us-intl ; } ; showkeybintl () { showkbintl ; }
+showkbus () { gkbd-keyboard-display -l us ; } ; showkeybintl () { showkbus ; }
