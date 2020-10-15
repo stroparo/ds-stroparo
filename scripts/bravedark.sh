71ce990d87e2edda03d0980988bcc2c169831e50
Cristian Stroparo <stroparo@outlook.com>
2020-10-15 19:27:16 -0300

    Update brave-browser launcher script

--- scripts/bravedark.sh
+++ scripts/bravedark.sh
@@ -6,8 +6,6 @@ fi
 
 if ! (ps -ef | grep -i -w 'brave-browser' | grep -v grep) ; then
   brave-browser ${BRAVE_USER_DATA_DIR:+--user-data-dir=${BRAVE_USER_DATA_DIR}} \
-    --enable-features=WebUIDarkMode \
-    --force-dark-mode \
     &
   disown
 fi
