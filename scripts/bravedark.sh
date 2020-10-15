6e90a40f0d3416bbee8e914eee313dbfabc641f5
Cristian Stroparo <stroparo@outlook.com>
2020-10-15 19:14:39 -0300

    Fix bravedark.sh

--- scripts/bravedark.sh
+++ scripts/bravedark.sh
@@ -4,8 +4,8 @@ if [ -n "${BRAVE_USER_DATA_DIR}" ] && [ ! -d "${BRAVE_USER_DATA_DIR}" ] ; then
   mkdir -p "${BRAVE_USER_DATA_DIR}"
 fi
 
-if ! (ps -ef | grep -i -w 'brave' | grep -v grep) ; then
-  brave ${BRAVE_USER_DATA_DIR:+--user-data-dir=${BRAVE_USER_DATA_DIR}} \
+if ! (ps -ef | grep -i -w 'brave-browser' | grep -v grep) ; then
+  brave-browser ${BRAVE_USER_DATA_DIR:+--user-data-dir=${BRAVE_USER_DATA_DIR}} \
     --enable-features=WebUIDarkMode \
     --force-dark-mode \
     &
