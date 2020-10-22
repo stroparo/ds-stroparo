4736954ee7166f8348ba718947656e343c171cfb
Cristian Stroparo <stroparo@outlook.com>
2020-10-22 16:46:27 -0300

    Remove --use-gl option in mychrome

--- scripts/mychrome.sh
+++ scripts/mychrome.sh
@@ -8,7 +8,6 @@ if which google-chrome >/dev/null 2>&1 \
   && ! (ps -ef | grep -i -w 'google-chrome' | grep -v grep)
 then
   google-chrome ${STUDD:+--user-data-dir=${STUDD}} \
-    --use-gl=desktop \
     &
   disown
 fi
