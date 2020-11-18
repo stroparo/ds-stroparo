46aa8cbe1780687616e0f64487c5e1f18bcdd241
Cristian Stroparo <stroparo@outlook.com>
2020-11-17 22:58:47 -0300

    Remove all CZ_REPO* stuff

--- recipes/st-conf-git.sh
+++ recipes/st-conf-git.sh
@@ -18,36 +18,12 @@ clonemygits "$STGITS"
 (cd "$DEV" ; MYEMAIL="stroparo@outlook.com" confgits $(echo ${STGITS_BASENAMES})
 )
 
-# #############################################################################
-# Other custom repos
-
-_clone_custom_cz_repos () {
-  : ${CZ_REPOS_DIRNAME_URL:=https://stroparo@bitbucket.org/stroparo/}; export CZ_REPOS_DIRNAME_URL
-
-  # Fetch for-loop elements from the (@@@) suffixes of variable names of the form 'CZ_REPO_@@@':
-  for repo in \
-    $(declare -x | grep -w 'CZ_REPO_[^=]*=' | grep -v '_RXPR' | sed -e 's/^declare -x CZ_REPO_//' -e 's/=.*$//')
-  do
-    # Map the 'CZ_REPO_@@@' variable name's suffix i.e. @@@ to the final basename
-    # ... in the URL to be cloned, such mappings being sed expressions in the
-    # ... respective CZ_REPO_@@@_RXPR global variables:
-    repo_basename="$(echo "${repo}" | tr '[[:upper:]]' '[[:lower:]]' | eval "sed -e \"\${CZ_REPO_${repo}_RXPR}\"")"
-
-    if eval "test ! -d \"\${CZ_REPO_${repo}}\"" ; then
-      eval "git clone \"${CZ_REPOS_DIRNAME_URL%/}/${repo_basename#/}.git\" \"\${CZ_REPO_${repo}}\""
-    fi
-  done
-}
-
-_clone_custom_cz_repos
-
 
 # #############################################################################
 echo "${PROGNAME}: INFO: Adding mirrors..."
 
 if ${ST_DO_GIT_MIRRORING:-false} ; then
   stgitmirrorgithub.sh $(echo "${STGITS_BASENAMES}" | sed "s#^#${DEV:-${HOME}/workspace}/#")
-  stgitmirrorgithub.sh $(declare -x | grep -w 'CZ_REPO_[^=]*=' | grep -v '_RXPR' | sed -e 's/[^=]*=//' | tr -d '"')
 fi
 
 echo
@@ -60,8 +36,7 @@ gitremotepatternreplace -v -r "mirror" "https://stroparo@\([^/]*\)/stroparo/" "g
 gitremotepatternreplace -v -r "origin" "https://stroparo@\([^/]*\)/stroparo/" "git@\\1:stroparo/" "${DEV}"/*/
 
 for repo in \
-  "${DEV}"/*/ \
-  $(declare -x | grep -w 'CZ_REPO_[^=]*=' | grep -v '_RXPR' | sed -e 's/[^=]*=//' | tr -d '"')
+  "${DEV}"/*/
 do
   gittrackremotebranches -r "origin" "${repo}" "master" "develop"
 done
