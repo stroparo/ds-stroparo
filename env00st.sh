b1a0cb321dfda08426ea358c831a0ac9b2772567
Cristian Stroparo <stroparo@outlook.com>
2020-12-07 23:26:14 -0300

    Supress 'which' command output

--- env00st.sh
+++ env00st.sh
@@ -34,7 +34,7 @@ export EDITOR=vim
 export FZF_DEFAULT_COMMAND='ag --ignore .git --ignore "*.pyc" -g ""'
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 export GIT_EDITOR=vim
-export VISUAL="code" ; if ! which "${VISUAL}" ; then export VISUAL="subl" ; fi
+export VISUAL="code" ; if ! which "${VISUAL}" >/dev/null 2>&1 ; then export VISUAL="subl" ; fi
 
 # Path
 pathmunge -x "${HOME}/bin"
