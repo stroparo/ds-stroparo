fabdc1c88bb6837a92eebef6a3263318d2f68ca1
Cristian Stroparo <stroparo@outlook.com>
2020-12-13 19:54:28 -0300

    Streamline stcloseguiapps()

--- functions/stcloseguiapps.sh
+++ functions/stcloseguiapps.sh
@@ -1,17 +1,33 @@
 stcloseguiapps () {
-  typeset timeout=2
-  typeset applist="
-chrome
+  typeset timeout=4
+  typeset killnames="
 flameshot
-guake
 keepassxc keepassxc-proxy
 sublime_text plugin_host
-ulauncher
-xfce4-terminal
+xterm
 "
+  typeset killnamescheck="${killnames} code firefox fsearch guake xfce4-terminal"
+
+  # List of processes names which are not the command name (e.g. runs like <shell-filename> <file>):
+  typeset killpidsnamesregex="ulauncher"
+  typeset killpids="$(pgrep "${killpidsnamesregex}")"
+  typeset killpidsregex="$(echo ${killpids} | tr '\n' ' ' | tr -s ' ' '|' | sed -e 's/  *//')"
 
   firefoxclose
-  killall -HUP $(echo ${applist}) >/dev/null 2>&1
+  windowclose code 'Visual Studio Code'
+  windowclose fsearch
+
+  # Terminal emulators
+  guake -q
+  windowclose xfce4-terminal 'Linux Lite Terminal' 'ctrl+shift+q'
+
+  killall -HUP $(echo ${killnames}) >/dev/null 2>&1
+  kill $(echo ${killpids}) >/dev/null 2>&1
+
   sleep ${timeout}
-  ! pidof $(echo ${applist}) >/dev/null 2>&1
+
+  # Return value based on all apps having been closed:
+  ! pidof $(echo ${killnames}) >/dev/null 2>&1 \
+  && [ -z "$(ps -ef | awk "$2 ~ /${killpidsregex}/")" ]
+  return $?
 }
