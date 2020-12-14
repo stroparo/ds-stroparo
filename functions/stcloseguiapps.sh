ab8cb5dff3f7f0cc6b9fe8fba02bcb08a7124e48
Cristian Stroparo <stroparo@outlook.com>
2020-12-14 01:47:04 -0300

    Fix stcloseguiapps by checking app exists before trying to capture window

--- functions/stcloseguiapps.sh
+++ functions/stcloseguiapps.sh
@@ -13,13 +13,13 @@ xterm
   typeset killpids="$(pgrep "${killpidsnamesregex}")"
   typeset killpidsregex="$(echo ${killpids} | tr '\n' ' ' | tr -s ' ' '|' | sed -e 's/  *//')"
 
-  firefoxclose
-  windowclose code 'Visual Studio Code'
-  windowclose fsearch
+  if which code >/dev/null 2>&1 ; then windowclose code 'Visual Studio Code' ; fi
+  if which firefox >/dev/null 2>&1 ; then firefoxclose ; fi
+  if which fsearch >/dev/null 2>&1 ; then windowclose fsearch ; fi
 
   # Terminal emulators
   guake -q
-  windowclose xfce4-terminal 'Linux Lite Terminal' 'ctrl+shift+q'
+  if which xfce4-terminal; then windowclose xfce4-terminal 'Linux Lite Terminal' 'ctrl+shift+q' ; fi
 
   killall -HUP $(echo ${killnames}) >/dev/null 2>&1
   kill $(echo ${killpids}) >/dev/null 2>&1
