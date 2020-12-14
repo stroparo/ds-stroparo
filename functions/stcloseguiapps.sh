26aadcf12d47d57a48ec21cebef0852d2b6be02e
Cristian Stroparo <stroparo@outlook.com>
2020-12-14 02:11:03 -0300

    Move keepassxc to be killed with SIGTERM

--- functions/stcloseguiapps.sh
+++ functions/stcloseguiapps.sh
@@ -4,13 +4,12 @@ stcloseguiapps () {
 chrome
 flameshot
 fsearch
-keepassxc keepassxc-proxy
 sublime_text plugin_host
 "
   typeset killnamescheck="${killnames} code firefox guake xfce4-terminal"
 
   # List of processes names which are not the command name (e.g. runs like <shell-filename> <file>):
-  typeset killpidsnamesregex="ulauncher"
+  typeset killpidsnamesregex="keepassxc ulauncher"
   typeset killpids="$(pgrep "${killpidsnamesregex}")"
   typeset killpidsregex="$(echo ${killpids} | tr '\n' ' ' | sed -e 's/  *//' | tr -s ' ' '|')"
 
@@ -27,7 +26,7 @@ sublime_text plugin_host
   sleep ${timeout}
 
   # Return value based on all apps having been closed:
-  ! pidof $(echo ${killnamescheck}) >/dev/null 2>&1 \
-  && [ -z "$(ps -ef | awk "$2 ~ /${killpidsregex}/")" ]
+  ! pidof $(echo ${killnamescheck}) \
+    && [ -z "$(ps -ef | awk "$2 ~ /${killpidsregex}/")" ]
   return $?
 }
